4a6d4ac6be00656d8d70538f250a8f4094752142
Dave Stanton <davestanton.us@gmail.com>
2014-12-14 22:14:13 -0500

    made array

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by David Stanton on 12/7/14.
+//  Copyright (c) 2014 David Stanton. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named: "")
+}
